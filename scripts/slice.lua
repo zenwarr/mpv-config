8f09eefb4bec7bd1e63f2426a4c6c49b119049d5
zenwarr <zenw@yandex.ru>
2023-03-30 16:58:33 +0600

    slice: output encoding complete status to console

--- scripts/slice.lua
+++ scripts/slice.lua
@@ -119,6 +119,7 @@ local function cut()
         status_overlay.data = "{\\a0\\fs20\\c&HFF&}Error: " .. replace_newline_with_spaces(res.stderr)
     else
         log_cmd_output(res)
+        msg.info("Encoding completed (saved to " .. outpath .. ")")
         status_overlay.data = "{\\a0\\fs20\\c&HFF00&}Encoding completed (saved to " .. outpath .. ")"
     end
 
