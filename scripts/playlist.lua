530f23fbf1ef032f6cc6dd1b74b611186664d61f
zenwarr <zenw@yandex.ru>
2023-04-14 13:51:27 +0600

    remove common filename prefix in playlist

--- scripts/playlist.lua
+++ scripts/playlist.lua
@@ -5,10 +5,11 @@
     https://github.com/CogentRedTester/mpv-scroll-list
 ]]
 
-local mp = require 'mp'
+package.path = package.path .. ";" .. mp.command_native({ "expand-path", "~~/script-modules/?.lua" })
 
---adding the source directory to the package path and loading the module
-local list = dofile(mp.command_native({"expand-path", "~~/script-modules/scroll-list.lua"}))
+local mp = require 'mp'
+local utils = require 'mp.utils'
+local list = require 'scroll-list'
 
 --modifying the list settings
 list.header = "Playlist \\N ------------------------------------"
@@ -22,6 +23,62 @@ end
 
 table.insert(list.keybinds, { 'ENTER', 'open_item', open_item, {} })
 
+local function get_trimmed_array(arr, end_index)
+    local new_arr = {}
+    for i = 1, end_index do
+        new_arr[i] = arr[i]
+    end
+    return new_arr
+end
+
+local function split(inputstr, sep)
+    local result = {}
+
+    for str in inputstr:gmatch("([^" .. sep .. "]+)") do
+        table.insert(result, str)
+    end
+
+    return result
+end
+
+local function min(a, b)
+    if a < b then
+        return a
+    else
+        return b
+    end
+end
+
+local function get_dir(path)
+    local prefix, _ = utils.split_path(path)
+    return prefix
+end
+
+local function get_common_prefix_len(items)
+    local prefix = get_dir(items[1])
+
+    for i = 1, #items[1] do
+        local cur_item = items[i]
+        if cur_item == nil then
+            break
+        end
+
+        cur_item = get_dir(cur_item)
+
+        local prev_sep_index = -1
+        for ch_idx = 1, min(#prefix, #cur_item) do
+            if prefix:sub(ch_idx, ch_idx) ~= cur_item:sub(ch_idx, ch_idx) then
+                prefix = prefix:sub(1, prev_sep_index + 1)
+                break
+            elseif prefix:sub(ch_idx, ch_idx) == '/' then
+                prev_sep_index = ch_idx
+            end
+        end
+    end
+
+    return #prefix
+end
+
 local function reinit_list()
     local cur_item = mp.get_property_native('playlist-pos')
     local playlist = mp.get_property_native('playlist', {})
@@ -31,13 +88,21 @@ local function reinit_list()
         list.selected = cur_item + 1
     end
 
+    local titles = {}
+    for i = 1, #playlist do
+        titles[i] = playlist[i].title or playlist[i].filename
+    end
+
+    local common_prefix_len = get_common_prefix_len(titles)
+
     for i = 1, #playlist do
         local item = {}
         if cur_item ~= nil and i == cur_item + 1 then
             item.style = [[{\c&H33ff66&}]]
         end
 
-        item.ass = '\\h\\h\\h'..list.ass_escape(playlist[i].title or playlist[i].filename)
+        local item_text = titles[i]:sub(common_prefix_len)
+        item.ass = '\\h\\h\\h'..list.ass_escape(item_text)
         list.list[i] = item
     end
 
